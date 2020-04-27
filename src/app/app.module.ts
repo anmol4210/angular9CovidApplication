bcc0133070d591cdde4700dffa4e5dd716965e88
Anmol Narang <anmol.narang@nagarro.com>
2020-04-27 10:10:57 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,21 @@ import { HttpClientModule } from '@angular/common/http';
 // import { LoginInMemoryDataService } from './core/services/login-in-memory-data.service';
 import { PrecautionsModule } from 'src/app/precautions/precautions.module';
 import { LoginModule } from 'src/app/login/login.module';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { LoginInMemoryDataService } from './core/services/login-in-memory-data.service';
+
 // import {DashboardModule} from 'src/app/dashboard/dashboard.module'
 @NgModule({
   declarations: [AppComponent, PortalHeaderComponent, PortalFooterComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(LoginInMemoryDataService, {
+      dataEncapsulation: false,
+      passThruUnknownUrl: true,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,20 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { LoginInMemoryDataService } from '../core/services/login-in-memory-data.service';
 
 import { LoginService } from '../core/services/login.service';
 @NgModule({
   declarations: [LoginComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(LoginInMemoryDataService, {
-      dataEncapsulation: false,
-    }),
-  ],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   providers: [],
   exports: [FormsModule, LoginComponent],
 })
